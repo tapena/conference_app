42a4b4ac7b3e0cb016a87698b57d372fa65bfb7a
Tatiana Pena <Tatiana.c.pena1@gmail.com>
2018-09-06 13:22:17 -0500

    09.05.18

---
+++ app/controllers/api/conference_examples_controller.rb
@@ -0,0 +1,2 @@
+class Api::ConferenceExamplesController < ApplicationController
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_04_221312) do
+ActiveRecord::Schema.define(version: 2018_09_06_160654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "meetings", force: :cascade do |t|
+    t.string "title"
+    t.text "agenda"
+    t.string "time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "speakers", force: :cascade do |t|
     t.string "first_name"
